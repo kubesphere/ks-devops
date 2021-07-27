0ff60020681a241a17fd4cfa6d16e73f3ff804af
rick <1450685+LinuxSuRen@users.noreply.github.com>
2021-07-27 17:01:34 +0800

    Add info log output when the Sonarqube integration is disabled

--- pkg/kapis/devops/v1alpha2/register.go
+++ pkg/kapis/devops/v1alpha2/register.go
@@ -57,7 +57,7 @@ func AddToContainer(container *restful.Container, ksInformers externalversions.S
 		return err
 	}
 
-	err = AddSonarToWebService(ws, devopsClient, sonarqubeClient, k8sClient)
+	err = addSonarqubeToWebService(ws, devopsClient, sonarqubeClient, k8sClient)
 	if err != nil {
 		return err
 	}
@@ -633,33 +633,6 @@ func AddPipelineToWebService(webservice *restful.WebService, devopsClient devops
 			Reads(devops.ReqJenkinsfile{}).
 			Returns(http.StatusOK, api.StatusOK, map[string]interface{}{}).
 			Writes(map[string]interface{}{}))
-	}
-	return nil
-}
-
-func AddSonarToWebService(webservice *restful.WebService, devopsClient devops.Interface, sonarClient sonarqube.SonarInterface,
-	k8sClient k8s.Client) error {
-	sonarEnable := devopsClient != nil && sonarClient != nil
-	if sonarEnable {
-		sonarHandler := NewPipelineSonarHandler(devopsClient, sonarClient, k8sClient)
-		webservice.Route(webservice.GET("/devops/{devops}/pipelines/{pipeline}/sonarstatus").
-			To(sonarHandler.GetPipelineSonarStatusHandler).
-			Doc("Get the sonar quality information for the specified pipeline of the DevOps project. More info: https://docs.sonarqube.org/7.4/user-guide/metric-definitions/").
-			Metadata(restfulspec.KeyOpenAPITags, []string{constants.DevOpsPipelineTag}).
-			Param(webservice.PathParameter("devops", "DevOps project's ID, e.g. project-RRRRAzLBlLEm")).
-			Param(webservice.PathParameter("pipeline", "the name of pipeline, e.g. sample-pipeline")).
-			Returns(http.StatusOK, api.StatusOK, []sonarqube.SonarStatus{}).
-			Writes([]sonarqube.SonarStatus{}))
-
-		webservice.Route(webservice.GET("/devops/{devops}/pipelines/{pipeline}/branches/{branch}/sonarstatus").
-			To(sonarHandler.GetMultiBranchesPipelineSonarStatusHandler).
-			Doc("Get the sonar quality check information for the specified pipeline branch of the DevOps project. More info: https://docs.sonarqube.org/7.4/user-guide/metric-definitions/").
-			Metadata(restfulspec.KeyOpenAPITags, []string{constants.DevOpsPipelineTag}).
-			Param(webservice.PathParameter("devops", "DevOps project's ID, e.g. project-RRRRAzLBlLEm")).
-			Param(webservice.PathParameter("pipeline", "the name of pipeline, e.g. sample-pipeline")).
-			Param(webservice.PathParameter("branch", "branch name, e.g. master")).
-			Returns(http.StatusOK, api.StatusOK, []sonarqube.SonarStatus{}).
-			Writes([]sonarqube.SonarStatus{}))
 	}
 	return nil
 }
