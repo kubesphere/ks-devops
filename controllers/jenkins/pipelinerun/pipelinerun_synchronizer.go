6a7d6eab4e60b2f87fb96b30530f5aa8df9e8ef0
johnniang <johnniang@fastmail.com>
2021-09-26 18:11:45 +0800

    Extract PipelineRun finder into separated file

--- controllers/jenkins/pipelinerun/pipelinerun_synchronizer.go
+++ controllers/jenkins/pipelinerun/pipelinerun_synchronizer.go
@@ -33,39 +33,6 @@ type SyncReconciler struct {
 	JenkinsCore core.JenkinsCore
 }
 
-type pipelineRunIdentity struct {
-	id      string
-	refName string
-}
-
-type pipelineRunFinder map[pipelineRunIdentity]*v1alpha3.PipelineRun
-
-func (finder pipelineRunFinder) find(run *job.PipelineRun, isMultiBranch bool) (*v1alpha3.PipelineRun, bool) {
-	identity := pipelineRunIdentity{
-		id: run.ID,
-	}
-	if isMultiBranch {
-		identity.refName = run.Pipeline
-	}
-	pipelineRun, ok := finder[identity]
-	return pipelineRun, ok
-}
-
-func newPipelineRunFinder(pipelineRuns []v1alpha3.PipelineRun) pipelineRunFinder {
-	// convert PipelineRuns to map[pipelineRunIdentity]PipelineRun
-	finder := pipelineRunFinder{}
-	for _, pipelineRun := range pipelineRuns {
-		pipelineRunIdentity := pipelineRunIdentity{
-			id: pipelineRun.Annotations[v1alpha3.JenkinsPipelineRunIDKey],
-		}
-		if pipelineRun.Spec.SCM != nil {
-			pipelineRunIdentity.refName = pipelineRun.Spec.SCM.RefName
-		}
-		finder[pipelineRunIdentity] = &pipelineRun
-	}
-	return finder
-}
-
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 func (r *SyncReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
