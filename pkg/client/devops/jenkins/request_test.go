bc93a2bea8d434bfc6c7f0bbcaa60a05a8aa1670
gaffeyQiu <253896514@qq.com>
2022-01-12 22:13:59 +0800

    test: add request_test

---
+++ pkg/client/devops/jenkins/request_test.go
@@ -0,0 +1,38 @@
+package jenkins
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func newFakeRequester() *Requester {
+	// init log
+	new(Jenkins).initLoggers()
+
+	return &Requester{
+		Base: "localhost",
+		Client: http.DefaultClient,
+	}
+}
+
+func newFakeAPIRequest() *APIRequest {
+	return NewAPIRequest("POST", "/test", nil)
+}
+
+func TestRequesterDo(t *testing.T) {
+	// test upload fail logic
+	requester := newFakeRequester()
+	fileNames := []string{"a.tmp", "b.tmp"}
+	_, err := requester.Do(newFakeAPIRequest(), nil, fileNames)
+	assert.NotNil(t, err)
+}
+
+func TestRequesterDoGet(t *testing.T) {
+	// test upload fail logic
+	requester := newFakeRequester()
+	fileNames := []string{"a.tmp", "b.tmp"}
+	_, err := requester.DoGet(newFakeAPIRequest(), nil, fileNames)
+	assert.NotNil(t, err)	
+}
