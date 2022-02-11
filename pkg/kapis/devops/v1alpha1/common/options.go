02590ffe6bb001bf4e1bfb7684fc3e974c6a495c
johnniang <johnniang@fastmail.com>
2022-02-11 10:43:45 +0800

    Fix lint error caused by license-eye fix
    Signed-off-by: johnniang <johnniang@fastmail.com>

--- pkg/kapis/devops/v1alpha1/common/options.go
+++ pkg/kapis/devops/v1alpha1/common/options.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
 package common
 
 import "sigs.k8s.io/controller-runtime/pkg/client"
