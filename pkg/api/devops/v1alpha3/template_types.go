6f46b763ef1a22850da5a98a283089642a1cffe3
johnniang <johnniang@fastmail.com>
2022-03-08 14:44:46 +0800

    Add required field into template parameter
    Signed-off-by: johnniang <johnniang@fastmail.com>

--- pkg/api/devops/v1alpha3/template_types.go
+++ pkg/api/devops/v1alpha3/template_types.go
@@ -48,6 +48,10 @@ type TemplateParameter struct {
 	//+optional
 	Description string `json:"description,omitempty"`
 
+	// Required indicates if this parameter is mandatory.
+	//+optional
+	Required bool `json:"required"`
+
 	// Default is default value of the parameter.
 	//+optional
 	Default apiextensionv1.JSON `json:"default,omitempty"`
