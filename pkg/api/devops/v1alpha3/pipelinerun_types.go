93c3afd636f8aede7605de77a88f90a75c60926f
jackyu <jackyu@yunify.com>
2023-02-01 16:39:45 +0800

    add printer-column(ID) for pipelinerun

--- pkg/api/devops/v1alpha3/pipelinerun_types.go
+++ pkg/api/devops/v1alpha3/pipelinerun_types.go
@@ -76,6 +76,7 @@ type PipelineRunStatus struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="ID",type=string,JSONPath=`.metadata.annotations.devops\.kubesphere\.io/jenkins-pipelinerun-id`,description="The id of a PipelineRun"
 // +kubebuilder:printcolumn:name="Phase",type=string,JSONPath=`.status.phase`,description="The phase of a PipelineRun"
 // +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`,description="The age of a PipelineRun"
 // +kubebuilder:resource:shortName="pr",categories="devops"
